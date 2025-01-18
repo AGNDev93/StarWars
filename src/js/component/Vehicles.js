52ef85386a436aed7963338c2c5bf67b2bf9dc50
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2025-01-18 02:08:55 +0000

    Color títulos

--- src/js/component/Vehicles.js
+++ src/js/component/Vehicles.js
@@ -11,7 +11,7 @@ const Vehicles = () => {
     console.log(store.vehicles)
     return (
         <div style={{ paddingBottom: "70px" }}>
-            <h1 className="d-flex flex-start mb-4 ms-5 pt-3" style={{color:"#F5F5DC"}}>Vehicles</h1>
+            <h1 className="d-flex flex-start mb-4 ms-5 pt-3" style={{color:"#FFF5E1"}}>Vehicles</h1>
             <div className="d-flex flex-row overflow-scroll ms-5 me-5">
                 {store.vehicles.map((item, index) => (
                     <div key={index} style={{ display: 'inline-block', marginRight: '25px' }}>
